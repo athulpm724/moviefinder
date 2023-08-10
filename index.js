09d84e88a2aa0b957efd089df4698db162e03e1c
unknown <athulpm724@gmail.com>
2023-08-10 14:13:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function search() {
+    title = movieName.value;
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=a1bbc6fd&t=${title}`).then(data => data.json()).then(wdata => display(wdata));
+
+    console.log(title);
+
+    function display(data){
+
+    
+        // temperature
+        release = data.Released;
+        genre = data.Genre;
+        imdb=data.imdbRating;
+
+        dataResult=`<p>RELEASE DATE : ${release}<br>GENRE : ${genre} <br> IMDB RATING : ${imdb}</p>`;
+
+        result.innerHTML=dataResult;
+    }    
+}
